a3a4253c71eb32d022c4d867e8f179e984f06626
Kevin <Harmonixer00@aim.com>
2020-11-26 12:17:28 -0500

    Added computerPlay function. Added notes to JS file and test variables.

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+
+//Randomly generates what the computer will output.
 function computerPlay(){
-    
-}
+    let result = Math.floor(Math.random() * 3);
+    if(result === 0){
+        result = 'Rock';
+    }
+    else if(result === 1){
+        result = 'Paper';
+    }
+    else{
+        result = 'Scissors';
+    }
+    return result;
+  }
+
+  //Test variables
+var kevin = 10;
+var chris = 20;
